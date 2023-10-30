1c636414fad7f899ca4b24ba54c5f079c045a5c1
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2023-10-30 16:56:37 +0100

    First attempt to migrate on Spring Boot 3
    - Remove org.zalando.problem.Problem usage and replace it by "native" ProblemDetail Spring boot entity
    - Upgrade detekt.yaml rules
    - Update gradle-wrapper.properties from 7.4.2 to 8.4
    - Rework CsmAzureSecurityConfiguration with filterChain Bean standard instead of configure method
    - aadJwtAuthenticationConverter has been removed and mechanism is now based on jwtGrantedAuthoritiesConverter
    - Update all dependencies especially com.azure* to com.azure.spring*
    - jwsUri is now based on new AadAuthenticationProperties (appIdUri and profile.*)
    - Spotless fixed

--- src/main/kotlin/com/cosmotech/api/azure/AzureExceptionHandling.kt
+++ src/main/kotlin/com/cosmotech/api/azure/AzureExceptionHandling.kt
@@ -6,31 +6,27 @@ import com.azure.storage.blob.models.BlobStorageException
 import com.cosmotech.api.exceptions.CsmExceptionHandling
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.context.request.NativeWebRequest
-import org.zalando.problem.Problem
-import org.zalando.problem.Status
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
 private const val HTTP_STATUS_CODE_CONFLICT = 409
 
-@ControllerAdvice
+@RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 internal class AzureExceptionHandling : CsmExceptionHandling() {
 
-  override fun isCausalChainsEnabled() = true
-
   @ExceptionHandler
-  fun handleBlobStorageException(
-      exception: BlobStorageException,
-      request: NativeWebRequest
-  ): ResponseEntity<Problem> {
+  fun handleBlobStorageException(exception: BlobStorageException): ProblemDetail {
     val status =
         when (exception.statusCode) {
-          HTTP_STATUS_CODE_CONFLICT -> Status.CONFLICT
-          else -> Status.INTERNAL_SERVER_ERROR
+          HTTP_STATUS_CODE_CONFLICT -> HttpStatus.CONFLICT
+          else -> HttpStatus.INTERNAL_SERVER_ERROR
         }
-    return create(status, exception, request)
+    if (exception.message == null) {
+      return ProblemDetail.forStatus(status)
+    }
+    return ProblemDetail.forStatusAndDetail(status, exception.message!!)
   }
 }
--- src/main/kotlin/com/cosmotech/api/azure/adx/AzureDataExplorerClient.kt
+++ src/main/kotlin/com/cosmotech/api/azure/adx/AzureDataExplorerClient.kt
@@ -7,16 +7,16 @@ import com.cosmotech.api.events.CsmEventPublisher
 import com.cosmotech.api.events.ScenarioRunEndTimeRequest
 import com.cosmotech.api.scenariorun.DataIngestionState
 import com.microsoft.azure.kusto.data.Client
-import com.microsoft.azure.kusto.data.ClientImpl
+import com.microsoft.azure.kusto.data.ClientFactory
 import com.microsoft.azure.kusto.data.ClientRequestProperties
 import com.microsoft.azure.kusto.data.KustoOperationResult
 import com.microsoft.azure.kusto.data.auth.ConnectionStringBuilder
 import com.microsoft.azure.kusto.data.exceptions.DataServiceException
+import jakarta.annotation.PostConstruct
 import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit
 import java.time.temporal.Temporal
 import java.util.concurrent.TimeUnit
-import javax.annotation.PostConstruct
 import org.slf4j.LoggerFactory
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
@@ -69,7 +69,7 @@ class AzureDataExplorerClient(
 
   @PostConstruct
   internal fun init() {
-    this.kustoClient = ClientImpl(getConnectionStringBuilder(baseUri))
+    this.kustoClient = ClientFactory.createClient(getConnectionStringBuilder(baseUri))
   }
 
   internal fun setKustoClient(kustoClient: Client) {
