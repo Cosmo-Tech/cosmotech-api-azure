34fd120e308927376596a672a4beefaa9736a180
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2024-03-27 20:40:52 +0100

    Test API Key

--- src/main/kotlin/com/cosmotech/api/azure/CsmAzureSecurityConfiguration.kt
+++ src/main/kotlin/com/cosmotech/api/azure/CsmAzureSecurityConfiguration.kt
@@ -20,6 +20,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.Jwt
@@ -29,6 +31,7 @@ import org.springframework.security.oauth2.jwt.JwtTimestampValidator
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.util.StringUtils
 
@@ -71,11 +74,23 @@ internal open class CsmAzureSecurityConfiguration(
             organizationUserGroup,
             organizationViewerGroup,
             csmPlatformProperties)
+        .httpBasic()
+        .and()
         .oauth2ResourceServer { oauth2 ->
           oauth2.jwt { jwt -> run { jwt.jwtAuthenticationConverter(jwtAuthenticationConverter) } }
         }
 
     return http.build()
+  }
+  @Bean
+  open fun userDetailsService(): InMemoryUserDetailsManager {
+    val user: UserDetails =
+        User.withDefaultPasswordEncoder()
+            .username("aamir")
+            .password("123")
+            .roles("Platform.Admin")
+            .build()
+    return InMemoryUserDetailsManager(user)
   }
 
   @Bean
